a0ab2cbeb54c9dd2832828e1f1c47e2267c361b6
ranjith <ranjithkankati2@gmail.com>
2025-10-29 20:26:28 +0530

    added items to the section

--- zepto/zeptoClone/src/App.jsx
+++ zepto/zeptoClone/src/App.jsx
@@ -4,12 +4,56 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
+  const items = [
+    {
+      id: 1,
+      name: 'Kothi meera',
+      price: 20
+    },
+    {
+      id: 2,
+      name: 'Bendakaya',
+      price: 50
+    },
+    {
+      id: 3,
+      name: 'Dhondakaya',
+      price: 40
+    },
+    {
+      id: 4,
+      name: 'curry leaves',
+      price: 15
+    },
+    {
+      id: 5,
+      name: 'Tomato',
+      price: 45
+    },
+    {
+      id: 6,
+      name: 'Potato',
+      price: 45
+    },
+    {
+      id: 7,
+      name: 'Eggs',
+      price: 60
+    }
+  ]
   return (
     <>
-      <div>
-        Zepto Clone
+      <div className='main-container'>
+        {items.map((item) => {
+          return (
+            <div className='card'>
+              <p>
+                {item.name}
+              </p>
+            </div>
+          )
+        })}
       </div>
     </>
   )
